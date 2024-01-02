a012e19d94b19129f4196f77159aa44cb60abcb5
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-02 18:10:54 +0700

    Buoi1

---
+++ array_method/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buoi1-Array_Method</title>
+</head>
+<body>
+    <h1>
+        Ôn tập kiến thức về mảng PHP
+    </h1>
+    <?php include "./array_method.php";?>
+</body>
+</html>
