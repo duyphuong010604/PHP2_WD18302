3a07df1ec83c9ff3b4761d5f3074c4e40e6aa96c
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-09 18:07:35 +0700

    lab2

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 require_once "vendor/autoload.php";
 
 use Php2\Oop\Model\Database;
+use Php2\Oop\Model\BaseModel;
+use Php2\Oop\Controller\BaseController;
 
 $name = new Database("hehehe");
-
-use Php2\Oop\Model\BaseModel;
 
 $basemodel = new BaseModel();
 
 var_dump($basemodel);
-
-use Php2\Oop\Controller\BaseController;
 
 $basecontroller = new BaseController();
 
