d90dec377c817f3a4beca4ef72895c89630141de
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-14 15:46:57 +0700

    Mau cho cac lab

--- src/Core/ConBase.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Php2\Oop\Core;
-
-class ConBase extends Base
-{
-
-}
--- src/Reponsitories/ModelInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Php2\Oop\Reponsitories;
-
-interface ModelInterface
-{
-    public function get_all();
-}
