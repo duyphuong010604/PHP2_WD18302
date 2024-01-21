77d37e36a30c37bc457292f8db877d3dcff86a80
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-21 23:29:15 +0700

    u

--- src/Core/Route.php
+++ src/Core/Route.php
@@ -2,9 +2,42 @@
 
 namespace Php2\Oop\Core;
 
+use Exception;
+
 class Route
 {
     protected array $routes;
+
+    public function __construct()
+    {
+
+    }
+    public function resolve(string $requesUrl)
+    {
+
+        $route = explode('?', $requesUrl)[0];
+        $action = $this->routes[$route] ?? null;
+
+        if (!$action) {
+            throw new Exception("This is an exception");
+        }
+
+        if (is_callable($action)) {
+            return call_user_func($action);
+        }
+
+        if (is_array($action)) {
+            [$class, $method] = $action;
+            if (class_exists($class)) {
+                $class = new $class();
+                if (method_exists($class, $method)) {
+                    return call_user_func_array([$class, $method], []);
+                }
+            }
+        }
+
+    }
+
 
     public function register(string $route, $action)
     {
@@ -14,30 +47,5 @@ class Route
         return $this;
     }
 
-    public function resolve(string $requesUrl)
-    {
-
-        $route = explode('?',$requesUrl)[0];
-        $action = $this->routes[$route] ?? null;
-
-        if(!$action){
-            throw new RouteNotFoundException();
-        }
-
-        if(is_callable($action)){
-            return call_user_func($action);
-        }
-
-        if(is_array($action)){
-            [$class, $method] = $action;
-            if(class_exists($class)){
-                $class = new $class();
-                if(method_exists($class,$method)){
-                    return call_user_func_array([$class,$method],[]);
-                }
-            }
-        }
-
-    }
 
 }
