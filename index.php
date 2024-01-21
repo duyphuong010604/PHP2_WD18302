1a676a7f04cd72e2c459454698c30d58ca3a5b27
duyphuong010604 <voduyphuong13@gmail.com>
2024-01-21 22:28:41 +0700

    push

--- index.php
+++ index.php
@@ -1,51 +1,9 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once "vendor\autoload.php";
 
-use Php2\Oop\Model\Database;
-use Php2\Oop\Model\BaseModel;
-use Php2\Oop\Controller\BaseController;
-use Php2\Oop\Model\Nhanvien;
-use Php2\Oop\Core\Base;
-use Php2\Oop\Core\ConBase;
+define ("ROOT_URL", "127.0.0.1:5000");
 
-$ConBase = new ConBase();
-$name = new Database("hehehe");
-$basemodel = new BaseModel();
-$basecontroller = new BaseController();
-$nhanvien = new Nhanvien();
-$Base = new Base();
+use App\Core\Route;
 
-echo $name->__construct("Vo Duy Phuong");
-echo "<br/>";
-var_dump($basemodel);
-echo $basemodel->get_name();
-echo "<br/>";
-var_dump($basecontroller);
-echo $basecontroller->get_controller();
-
-echo "<br/> Buoi 5 ne!";
-echo $basemodel->get_name();
-echo $nhanvien->get_name();
-echo $nhanvien->set_name("Phi Hao");
-
-echo "Sai static: " . $basemodel->get_idNhanvien();
-
-echo $basemodel::URL;
-
-echo $nhanvien::URL;
-
-
-echo $nhanvien->get_idNhanvien();
-
-echo "<br/> ---------Buoi 6 ne!---------<br/> ";
-var_dump($Base);
-echo $Base->get_name();
-echo $Base::Pi;
-echo $Base::$_Apolo;
-echo $Base->get_Apollo();
-echo $Base->set_name("Phi Hao");
-
-
-echo $ConBase::$_Apolo;
-
+new Route;
