dd0c84984911eeade4ff3d440f223a9e2f331e54
duyphuong010604 <voduyphuong13@gmail.com>
2024-02-03 07:49:37 +0700

    u

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -39,39 +39,40 @@ class LoginController extends BaseController
     public function handleLogin()
     {
 
-        $user = $_POST["email"];
-        $password = trim($_POST['password']);
-        $check = $this->lengthValidation(3, 7, $password);
-        if ($this->lengthValidation(3, 7, $password) === false) {
-            $this->errors['password'] = "ko dc de trong";
-            $this->_renderBase->renderHeader();
-            $this->load->render('layouts/client/pages/login', $this->errors);
-            $this->_renderBase->renderFooter();
-        } else {
-            $this->resetErrors();
-            echo "oki r do";
-            var_dump($this->errors);
-        }
+        // $user = $_POST["email"];
+        // $password = trim($_POST['password']);
+        // $check = $this->lengthValidation(3, 7, $password);
+        // if ($this->lengthValidation(3, 7, $password) === false) {
+        //     $this->errors['password'] = "ko dc de trong";
+        //     $this->_renderBase->renderHeader();
+        //     $this->load->render('layouts/client/pages/login', $this->errors);
+        //     $this->_renderBase->renderFooter();
+        // } else {
+        //     $this->resetErrors();
+        //     echo "oki r do";
+        //     var_dump($this->errors);
+        // }
 
         // validation form
         // kiểm tra trước rồi mới new UserModel();
-        // $userModel = new UserModel();
-        // $user = $userModel->checkUserExist($_POST["email"]);
-        // if(!$user){
-        //     // chuyển trang tới /?url=LoginController/loadViewLogin
-        //     // báo lỗi
-        // }
+        $userModel = new UserModel();
+        $user = $userModel->checkUserExist($_POST["email"]);
+        if (!$user) {
+            // chuyển trang tới /?url=LoginController/loadViewLogin
+            // báo lỗi
+        }
 
-        // //xác thực
-        // if(password_verify($_POST['password'], $user['password'])){
+        //xác thực
+        // if (password_verify($_POST['password'], $user['password'])) {
         //     // xử lý session
         //     $_SESSION['user'] = $user;
 
-        // }else{
-
         // }
+        else {
+            $_SESSION['user'] = $_POST["email"];
+        }
 
-        // var_dump($_SESSION['user']);
+        var_dump($_SESSION['user']);
 
     }
 
